5f5e901bc3fb94daad76cfd0bd1163f0603adf03
Nithin Reddy Jammula <nithinreddyjammula@Mac.lan>
2026-06-27 19:20:14 -0500

    feat(databricks): add databricks.yml and hello_world notebook to support bundle validation

---
+++ src/hello_world.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Hello World Notebook
+# MAGIC This notebook is deployed automatically using Databricks Asset Bundles (DABs) and GitHub Actions.
+
+# COMMAND ----------
+
+print("Hello from Databricks Asset Bundle!")
